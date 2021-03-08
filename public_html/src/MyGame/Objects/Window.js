a8b1e9a12beef2bc037390d40f904403a0f51e24
Weston Goggins <westongoggins26@gmail.com>
2021-03-07 16:15:02 -0800

    added functionality to window

--- public_html/src/MyGame/Objects/Window.js
+++ public_html/src/MyGame/Objects/Window.js
@@ -6,30 +6,39 @@
 
 //Is created with a renderable to display the viewport upon, the camera, the offset of the viewport from the edge of the renderable,
 //the width and height of the window, and the layer of the window in comparison to other windows
-function Window(renderableObject, camera ,xPos, yPos, offset, width, height, layer) {
+function Window(renderableObject, camera, offset, layer, drag, resize) {
     this.mRenderableObject = renderableObject;
     this.mCamera = camera;
-    this.getXform().setPosition(xPos, yPos);
     this.mOffset = offset;
-    this.getXform().setSize(width, height);
     this.mLayer = layer;
-    this.mDrag = false;
-    this.mResize = false;
+    this.mDrag = drag; 
+    this.mResize = resize;
     this.mVisible = true;
 }
 
 gEngine.Core.inheritPrototype(Window, GameObject);
 
 Window.prototype.initialize = function() {
-    
+    if (this.mDrag) {
+        
+    };
+    if (this.mResize) {
+        
+    };
 }
 
 Window.prototype.setDraggable = function(bool) {
     this.mDrag = bool;
+    if (this.mDrag) {
+        
+    };
 }
 
 Window.prototype.setResizeable = function(bool) {
-    this.mDraw = bool;
+    this.mResize = bool;
+    if (this.mResize) {
+        
+    };
 }
 
 Window.prototype.draw = function(camera) {
@@ -38,6 +47,11 @@ Window.prototype.draw = function(camera) {
     }
 }
 
+Window.prototype.getCamera = function() {
+    return this.mCamera;
+}
+
 Window.prototype.update = function () {
-    
+    //Sets viewport to position and size of renderable
+    this.mCamera.setViewport([this.getXform().getXpos(), this.getXform().getYpos(), this.getXform().getWidth(), this.getXform().getHeight()], this.mOffset);
 }
