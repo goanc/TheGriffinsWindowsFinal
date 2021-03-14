fbf0cd49d6e35565ec59aa4b27243fc231620c47
Weston Goggins <westongoggins26@gmail.com>
2021-03-14 16:04:40 -0700

    Added parameter descriptions

--- public_html/src/MyGame/Objects/Window.js
+++ public_html/src/MyGame/Objects/Window.js
@@ -4,15 +4,29 @@
  * A GameObject that ties a renderable and camera together to form a window object that can be optionally dragged and resized
  * 
  * FUNCTIONS:
- * Window(renderableObject, windowCam, xoffsetleft, xoffsetright, yoffsetbottom, yoffsettop, drag, resize):
- *      Defines a renderable with an attached camera, along with the camera's numerical left, right, bottom, and top offsets in relation to the renderable.
- *      Drag and resize are enabled if set to true, otherwise are disabled.
+ * Window(renderableObject, windowCam, xoffsetleft, xoffsetright, yoffsetbottom, yoffsettop, drag, resize):Defines a renderable with an attached camera
+ *      (renderableObject): A renderable object that will be associated with the camera.
+ *      (windowCam): The camera for the window. Dimensions will not stay the same.
+ *      (xoffsetleft): The camera's WC offset from the left side of renderableObject.
+ *      (xoffsetright): The camera's WC offset from the right side of renderableObject.
+ *      (yoffsetbottom): The camera's WC offset from the bottom side of renderableObject.
+ *      (yoffsettop): The camera's WC offset from the top side of renderableObject.
+ *      (drag): A boolean that allows the window to be draggable if set to true.
+ *      (resize): A boolean that allows the window to be resizeable if set to true.
  * initialize(): Initializes the draggable and resizable versions of renderable if enabled.
  * setISDrag(bool): Defines if the window is draggable or not, depending on whether the input is true or false.
+ *      (bool): A boolean that allows the window to be draggable if set to true.
  * setISResize(bool): Defines if the window is resizeable or not, depending on whether the input is true or false.
+ *      (bool): A boolean that allows the window to be resizeable if set to true.
  * getCamera(): Returns the window's camera.
+ * setKey(key): Sets the window's key.
+ *      (key): The window's numerical key.
+ * getKey(): Returns the window's numerical key.
  * draw(cam, objects): Draws the window on the inputted camera, and then draws the objects within the window on the inputted list of objects.
+ *      (cam): The world camera that this window will be displayed on.
+ *      (objects): An array of objects to be displayed within the window's camera.
  * update(cam): Draws the window object. Its dimensions are specified by the inputted world camera.
+ *      (cam): The world camera that the window's dimensions will compare to.
  */
 
 function Window(renderableObject, windowCam, xoffsetleft, xoffsetright, yoffsetbottom, yoffsettop, drag, resize) {
@@ -32,6 +46,7 @@ function Window(renderableObject, windowCam, xoffsetleft, xoffsetright, yoffsetb
     this.mVisible = true;
     this.mDraggable = null;
     this.mResizeable = null;
+    this.mKey = null;
 };
 
 gEngine.Core.inheritPrototype(Window, GameObject);
@@ -69,6 +84,14 @@ Window.prototype.setIsResize = function(bool) {
 Window.prototype.getCamera = function() {
     return this.mCamera;
 };
+
+Window.prototype.setKey = function(key) {
+    this.mKey = key;
+}
+
+Window.prototype.getKey = function() {
+    return this.mKey;
+}
 
 Window.prototype.draw = function(cam, objects) {
     if (this.mVisible) {
--- public_html/src/MyGame/Objects/WindowManager.js
+++ public_html/src/MyGame/Objects/WindowManager.js
@@ -4,20 +4,35 @@
  * An object that handles the layering of all window objects
  * 
  * FUNCTIONS:
- * WindowManager(): initializes the empty list of windows.
- * add(window, front): adds a window to the front of the list if 'front' is true, otherwise its added to the back.
- * remove(window): removes the inputted window from the list.
- * pushToFront(window): pushes the inputted window to the fron to list, layering it on top of other windows in the list.
- * draw(cam, objects): draws all windows in the inputted camera, and all windows display the inputted list of objects.
- * update(cam): updates all windows according to the specs of the inputted camera.
+ * WindowManager(): Initializes the empty array of windows.
+ * initialize(): Initializes the window key to 0.
+ * add(window, front): Adds a window to the front of the list if 'front' is true, otherwise its added to the back.
+ *      (window): The window to be added to the window list.
+ *      (front): A boolean that, if set to true, will layer the new window on top of all existing windows. Otherwise will be pushed to the back layer.
+ * remove(window): Removes the inputted window from the list.
+ *      (window): The window to be removed fromt the window list.
+ * pushToFront(window): Pushes the inputted window to the fron to list, layering it on top of other windows in the list.
+ *      (window): The window to be pushed in front of all other windows.
+ * draw(cam, objects): Draws all windows in the inputted camera, and all windows display the inputted list of objects.
+ *      (cam): The world camera that all winddows will be drawn upon.
+ *      (objects): An array of objects that the window cameras will draw.
+ * update(cam): Updates all windows according to the specs of the inputted camera.
+ *      (cam): The world camera that all the windows' dimensions will compare to.
  */
 
 function WindowManager() {
     this.mList = [];
+    this.mKey = null;
 }
 gEngine.Core.inheritPrototype(WindowManager, GameObject);
 
+WindowManager.prototype.initialize = function() {
+    this.mKey = 0;
+}
+
 WindowManager.prototype.add = function(window, front) {
+    window.setKey(this.mKey);
+    this.mKey++;
     if (front) {
         this.mList.unshift(window);
     }
@@ -28,7 +43,7 @@ WindowManager.prototype.add = function(window, front) {
 
 WindowManager.prototype.remove = function(window) {
     for (var i = this.mList.length - 1; i >= 0; i--) {
-        if (window === this.mList[i]) {
+        if (window.getKey() == this.mList[i].getKey()) {
             this.mList.splice(i, 1);
             i = -1;
         }
@@ -37,7 +52,7 @@ WindowManager.prototype.remove = function(window) {
 
 WindowManager.prototype.pushToFront = function (window) {
     for (var i = this.mList.length - 1; i >= 0; i--) {
-        if (window === this.mList[i]) {
+        if (window.getKey() == this.mList[i].getKey()) {
             this.mList.splice(i, 1);
             this.mList.unshift(window);
             i = -1;
